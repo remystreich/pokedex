2d2fa55878797ab2e14ab909c460e952600f58ec
remystreich <remystreich@gmail.com>
2023-06-28 12:16:37 +0200

    commit

--- index.php
+++ index.php
@@ -20,8 +20,7 @@ $userRepository = new UserRepository();
 $actionParts = isset($_SERVER['REQUEST_URI']) ? (str_replace(Config::$dirPath, '', $_SERVER['REQUEST_URI'])) : '';
 
 // Extraire l'action à partir de l'URL
-var_dump($actionParts);
-die;
+
 $action = explode('/' , $actionParts);
 
 
